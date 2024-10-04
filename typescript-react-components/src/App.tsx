dfda56d5480aca4bdad04ab4e8135d5c824ce22e
Khalil McFarlane <kmac36960@gmail.com>
2024-10-03 20:25:51 -0700

    Testing out props

--- typescript-react-components/src/App.tsx
+++ typescript-react-components/src/App.tsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-import Hello from './components/Hello';
-import Button from './components/Button';
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import Hello from "./components/Hello";
+import { SayName } from "./components/Hello";
+import Button from "./components/Button";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <Hello/>
-      <Button/>
+      <Hello />
+      <Button />
+      <SayName name="Bot" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- typescript-react-components/src/components/Hello.tsx
+++ typescript-react-components/src/components/Hello.tsx
@@ -1,16 +1,17 @@
-
-function sayName(name: string): React.JSX.Element {
-    return (
-        <div>
-          <h1>Hello ${name}!</h1>
-        </div>
-      );
+export function SayName({ name }: { name: string }): React.JSX.Element {
+  return (
+    <div>
+      <h1>Hi, my name is {name}!</h1>
+    </div>
+  );
 }
 
 function Hello(): React.JSX.Element {
+  let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <div>
       <h1>Hello Khalil!</h1>
+      <div id="Square">I'm going to square numbers from 1 to 10.</div>
     </div>
   );
 }
